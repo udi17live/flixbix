194d6a1f19a39ff15e3498fa3bb4ea1d01aac46c
Uditha Melan <melan.udi@gmail.com>
2020-06-28 14:17:19 +0530

    initial template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SingleMovieComponent } from './single-movie/single-movie.component';
+
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SingleMovieComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path:'', component: HomeComponent },
+      { path:'movies/:movieid/:slug', component: SingleMovieComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
